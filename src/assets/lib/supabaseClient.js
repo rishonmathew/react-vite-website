a3da846b0c97e06e619f3b3d0f70c91759153c51
rishon mathew <rishon.m2004@outlook.com>
2025-12-02 17:24:00 +1000

    Update supabaseClient.js

--- src/assets/lib/supabaseClient.js
+++ src/assets/lib/supabaseClient.js
@@ -1,1 +1,8 @@
-[ NO BACKEND CODE :( ]
+import { createClient } from "@supabase/supabase-js";
+
+const supabase = createClient(
+  import.meta.env.VITE_SUPABASE_URL,
+  import.meta.env.VITE_SUPABASE_ANON_KEY
+);
+
+export default supabase;
